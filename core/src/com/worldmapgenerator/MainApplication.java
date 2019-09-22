6ee4e6b7856b4855169eb5defd58464d371eeeab
DmitryFV <dima747@mail.ru>
2019-09-22 22:58:42 +0600

    [*] DesktopLauncher, MainApplication и другие - main.java переименован в MainApplication.java: название класса по стандартам Java должно быть с большой буквы, и просто main - не информативно, много дубликатов при поиске по имени (настройки gradle и т.п.)

---
+++ core/src/com/worldmapgenerator/MainApplication.java
@@ -0,0 +1,25 @@
+package com.worldmapgenerator;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.worldmapgenerator.Controller.SimplestVoronoiController;
+
+public class MainApplication extends ApplicationAdapter {
+
+    private SimplestVoronoiController controller;
+
+    @Override
+    public void create() {
+        controller = new SimplestVoronoiController();
+    }
+
+    @Override
+    public void render() {
+        controller.update(Gdx.graphics.getDeltaTime());
+    }
+
+    @Override
+    public void dispose() {
+        controller.dispose();
+    }
+}
--- desktop/src/com/worldmapgenerator/desktop/DesktopLauncher.java
+++ desktop/src/com/worldmapgenerator/desktop/DesktopLauncher.java
@@ -1,14 +1,13 @@
 package com.worldmapgenerator.desktop;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.worldmapgenerator.main;
+import com.worldmapgenerator.MainApplication;
 
 import java.awt.*;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.fullscreen = true;
 		config.vSyncEnabled = true;
@@ -16,6 +15,6 @@ public class DesktopLauncher {
 		config.width = screen.width;
 		config.height = screen.height;
 		config.samples = 5;
-		new LwjglApplication(new main(), config);
+		new LwjglApplication(new MainApplication(), config);
 	}
 }
